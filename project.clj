669dc43eb57276c071712d934dcdc6757d09252a
Avishai Ish-Shalom <avishai@fewbytes.com>
2015-02-04 02:13:48 +0200

    Added codox and midje

--- project.clj
+++ project.clj
@@ -4,5 +4,10 @@
   :license {:name "Apache v2"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [clojurewerkz/elastisch "2.1.0"]
-                 ])
+                 [clojurewerkz/elastisch "2.1.0"]]
+  :plugins [[codox "0.6.1"]]
+  :profiles {:dev {:dependencies [[clj-time "0.9.0"]
+                                  [riemann "0.2.8"]
+                                  [midje "1.6.3"]]}}
+  :codox {:src-linenum-anchor-prefix "L"
+          :src-dir-uri "https://github.com/avishai-ish-shalom/riemann-elasticsearch-plugin/blob/master"})
