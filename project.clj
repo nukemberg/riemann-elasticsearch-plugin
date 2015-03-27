5702f18d3ec1df73ae85ce93193cfd4290798f8f
Avishai Ish-Shalom <avishai@fewbytes.com>
2015-03-27 15:34:47 +0300

    Fixed deps

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Apache v2"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [clojurewerkz/elastisch "2.1.0"]]
+                 [clojurewerkz/elastisch "2.2.0-beta2" :exclusions [cheshire]]]
   :plugins [[codox "0.6.1"]
             [lein-midje "3.1.3"]]
   :profiles {:dev {:dependencies [[clj-time "0.9.0"]
