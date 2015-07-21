7f686204833ad4f104fcfe3f2621f6036cc3a1c7
Avishai Ish-Shalom <avishai@fewbytes.com>
2015-07-21 22:19:49 +0300

    updated deps for riemann 0.2.10

--- project.clj
+++ project.clj
@@ -3,12 +3,10 @@
   :url "https://github.com/avishai-ish-shalom/riemann-elasticsearch-output"
   :license {:name "Apache v2"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [clojurewerkz/elastisch "2.2.0-beta2" :exclusions [cheshire]]]
+  :dependencies [[clojurewerkz/elastisch "2.2.0-beta2" :exclusions [clj-http org.antlr/antlr-runtime potemkin cheshire]]]
   :plugins [[codox "0.6.1"]
             [lein-midje "3.1.3"]]
-  :profiles {:dev {:dependencies [[clj-time "0.9.0"]
-                                  [riemann "0.2.9"]
+  :profiles {:dev {:dependencies [[riemann "0.2.10"]
                                   [midje "1.6.3"]]}}
   :codox {:src-linenum-anchor-prefix "L"
           :src-dir-uri "https://github.com/avishai-ish-shalom/riemann-elasticsearch-plugin/blob/master"})
