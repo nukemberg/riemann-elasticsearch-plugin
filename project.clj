289dcea4bcc84018f7b45858f4fcfa864dd03189
Avishai Ish-Shalom <avishai@fewbytes.com>
2015-07-21 22:57:51 +0300

    Fixed deps issue

--- project.clj
+++ project.clj
@@ -6,7 +6,8 @@
   :dependencies [[clojurewerkz/elastisch "2.2.0-beta2" :exclusions [clj-http org.antlr/antlr-runtime potemkin cheshire]]]
   :plugins [[codox "0.6.1"]
             [lein-midje "3.1.3"]]
-  :profiles {:dev {:dependencies [[riemann "0.2.10"]
-                                  [midje "1.6.3"]]}}
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0"]
+				  [riemann "0.2.10"]
+                                  [midje "1.6.3" :exclusions [org.clojure/clojure]]]}}
   :codox {:src-linenum-anchor-prefix "L"
           :src-dir-uri "https://github.com/avishai-ish-shalom/riemann-elasticsearch-plugin/blob/master"})
